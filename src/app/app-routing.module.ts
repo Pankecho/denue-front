55e86c6468259d51c4ecabb7d4a6a4c774a94b34
Juan Pablo <juanpablomr.014@gmail.com>
2019-05-19 23:10:02 -0500

    Funcionalidad basica completada, falta borrado en lote y autenticacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {IndexComponent} from './index/index.component';
+import {DetailComponent} from './detail/detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'index',
+    component: IndexComponent
+  },
+  {
+    path: 'detail/:id',
+    component: DetailComponent
+  },
+  {path: '', redirectTo: 'login', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './shared/material/material.module';
+import { LoginComponent } from './login/login.component';
+import { IndexComponent } from './index/index.component';
+import { DetailComponent } from './detail/detail.component';
+import {ApiServiceService} from './shared/services/api-service.service';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { EmpresaModalComponent } from './empresa-modal/empresa-modal.component';
+import { UbicacionModalComponent } from './ubicacion-modal/ubicacion-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    IndexComponent,
+    DetailComponent,
+    EmpresaModalComponent,
+    UbicacionModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  entryComponents: [
+    EmpresaModalComponent,
+    UbicacionModalComponent
+  ],
+  providers: [ApiServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
